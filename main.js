ba2c760761185fccbfa5a278a4fd5724176e8ff6
waichungo <waichungojames@gmail.com>
2025-08-15 23:15:44 +0300

    Removed default menu bar

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow } = require('electron');
 const path = require('node:path');
 const isDev = require('electron-is-dev');
 const fs = require('node:fs');
+const { pathToFileURL } = require('url')
 
 function init() {
   const pdfjsDistPath = path.dirname(require.resolve('pdfjs-dist/package.json'));
@@ -13,14 +14,16 @@ init();
 let mainWindow;
 function createWindow() {
   mainWindow = new BrowserWindow({
+    autoHideMenuBar: true,
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-      webSecurity:false
+      webSecurity: false
     },
   });
-
+  var file = String.raw`C:\Users\James\Downloads\Documents\ Windell Oskay, Eric Schlaepfer - Open Circuits_ The Inner Beauty of Electronic Components (2022, No Starch Press) - libgen.li.pdf`
+  console.log(pathToFileURL(file).href)
   const startURL = isDev
     ? 'http://localhost:5579'
     : `file://${path.join(__dirname, '../build/index.html')}`;
